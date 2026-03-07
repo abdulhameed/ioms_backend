bbaa2f6c9cb3fc1c45c1556466e99806d1b8db16
Abdul <abdulhameedgiwa@gmail.com>
2026-03-07 11:13:39 +0100

    chore: add swagger doc

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -163,6 +163,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
     'EXCEPTION_HANDLER': 'apps.core.exceptions.custom_exception_handler',
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 # ── Simple JWT ────────────────────────────────────────────────────────────────
