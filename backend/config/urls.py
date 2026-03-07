3ce3f7351ce8ad7ab27de0957aca23f25f21352a
Abdul <abdulhameedgiwa@gmail.com>
2026-03-07 11:13:06 +0100

    chore: add swagger doc

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,6 +13,10 @@ urlpatterns = [
     path('api/v1/', include('apps.shortlets.urls')),
     path('api/v1/', include('apps.maintenance.urls')),
     path('api/v1/', include('apps.notifications.urls')),
+    # OpenAPI schema + docs
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 if settings.DEBUG:
